a0371d41b284a7491a377550b169c4deee668482
LiamTHCH <liam.thomaschollier@gmail.com>
2022-06-14 16:06:42 +0200

    ajout template db
    Signed-off-by: LiamTHCH <liam.thomaschollier@gmail.com>

--- DriveProject/DriveProject/settings.py
+++ DriveProject/DriveProject/settings.py
@@ -84,6 +84,16 @@ DATABASES = {
     }
 }
 
+###DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'dbtest', 
+#        'USER': 'postgres', 
+#        'PASSWORD': '1234',
+#        'HOST': '127.0.0.1', 
+#        'PORT': '5432',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
