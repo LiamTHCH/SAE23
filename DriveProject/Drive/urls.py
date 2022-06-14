910f9e6bc73a1aa60ff027a337c16aee69a5a24d
Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>
2022-06-14 23:00:48 +0200

    ajout basket avec view et change
    Signed-off-by: Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>

--- DriveProject/Drive/urls.py
+++ DriveProject/Drive/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
 from django.contrib import admin
 from . import views
-from Drive.views import index,product_detail,add_to_cart
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
 path('', views.index, name='index'), #racine
-path('admin/', admin.site.urls),
-path('product/<str:slug>', product_detail, name='product'),
-path('product/<str:slug>/add-to-cart/', add_to_cart, name='add-to-cart'),
-
-
-
-
+path('sh/<int:item>/', views.sh_item, name='show'),
+path('add/<int:item>/<int:qt>/', views.add_item, name='add_item'),
+path('sh/basket/', views.sh_basket, name='add_item'),
+path('ch/<int:item>/<int:qt>/', views.change_item, name='add_item'),
+path('del/<int:item>/', views.del_item, name='add_item'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- DriveProject/Drive/views.py
+++ DriveProject/Drive/views.py
@@ -2,18 +2,99 @@ import re
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from Drive.models import Product
-
-
+from employer.models import *
+import copy
+import ast
 # Create your views here.
+
+
+def getList(dict):
+    ls = []
+    try:
+        ls =  dict.keys()
+    except:
+        ls = []
+    return ls
+
 def index(request):
-    Product.objects.all()
-    return render(request,"indexc.html",context={"products":Product.objects.all()}) 
+    try:
+        print(request.session["cart"])
+    except:
+        request.session["cart"]
+    query = Produits.objects.all()
+    return render(request,"indexc.html",context={"products":query}) 
 
     
-def product_detail(request,slug):
-    product = get_object_or_404(Product,slug=slug)
-    return render(request,"detail.html",context={"product":product})
 
-def add_to_cart(request,slug):
-    pass
+def add_item(req,item,qt):
+    dic = req.session["cart"]
+    print(dic)
+    if req.method == 'POST':    
+        qt = req.POST.get('quantiter')
+        if req.POST.get('produit') is not None:
+            item = req.POST.get('produit')
+        item = Produits.objects.get(id=item)
+        item1 = copy.copy(str(item))
+        print(item)
+        if item1 in getList(dic):
+            dic[item1] = {"Amount": qt,"id": item.id,"price":float(item.prix)}
+        else:
+            dic[item1] = {"Amount": qt,"id": item.id,"price":float(item.prix)}
+    else:
+        item = Produits.objects.get(id=item)
+        item1 = copy.copy(str(item))
+        if item1 in getList(dic):
+            dic[item1] = {"Amount": qt,"id": item.id,"price":float(item.prix)}
+        else:
+            dic[item1] = {"Amount": 1,"id": item.id,"price":float(item.prix)}
+    req.session["cart"] = dic
+    return HttpResponseRedirect("/drive/sh/%s/"% item.id)
+
+
+
+def sh_item(req,item):
+    query = Produits.objects.get(id=item)
+    return render(req,"detail.html",{"product":query})
+
+
+def del_item(req,item):
+    item = Produits.objects.get(id=item)
+    dic = req.session["cart"]
+    item1 = copy.copy(str(item))
+    if item1 in getList(dic):
+            del dic[item1]
+    req.session["cart"] = dic
+    return HttpResponseRedirect("/drive/sh/basket/")
+
+def sh_basket(req):
+    if req.method == 'POST':  
+        Commandes.objects.create()
+    else:
+        items = req.session["cart"]
+        items = items.items()
+        return render(req,"basket_view.html",{"items":items})
+
+
+def change_item(req,item,qt):
+    dic = req.session["cart"]
+    print(dic)
+    if req.method == 'POST':    
+        qt = req.POST.get('quantiter')
+        if req.POST.get('produit') is not None:
+            item = req.POST.get('produit')
+        item = Produits.objects.get(id=item)
+        item1 = copy.copy(str(item))
+        print(item)
+        if item1 in getList(dic):
+            dic[item1] = {"Amount": qt,"id": item.id,"price":float(item.prix)}
+        else:
+            dic[item1] = {"Amount": qt,"id": item.id,"price":float(item.prix)}
+    else:
+        item = Produits.objects.get(id=item)
+        item1 = copy.copy(str(item))
+        if item1 in getList(dic):
+            dic[item1] = {"Amount": qt,"id": item.id,"price":float(item.prix)}
+        else:
+            dic[item1] = {"Amount": 1,"id": item.id,"price":float(item.prix)}
+    req.session["cart"] = dic
+    return HttpResponseRedirect("/drive/sh/basket/")
