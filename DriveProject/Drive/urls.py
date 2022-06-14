3e35ee1d141da8e7746fffd87ad95412e372122b
rayane <rayane.dib@uha.fr>
2022-06-14 14:37:33 +0200

    Ajout du panier

--- DriveProject/Drive/urls.py
+++ DriveProject/Drive/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib import admin
 from . import views
-from Drive.views import index,product_detail
+from Drive.views import index,product_detail,add_to_cart
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -9,6 +9,7 @@ urlpatterns = [
 path('', views.index, name='index'), #racine
 path('admin/', admin.site.urls),
 path('product/<str:slug>', product_detail, name='product'),
+path('product/<str:slug>/add-to-cart/', add_to_cart, name='add-to-cart'),
 
 
 
