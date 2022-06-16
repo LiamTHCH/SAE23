b46ac5853e7fa539deea41aeb9c798a24bbc7509
Mr-Tintin <esteban.descieux@free.fr>
2022-06-16 17:56:54 -0400

    FIX serv

--- DriveProject/DriveProject/urls.py
+++ DriveProject/DriveProject/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('drive/',include('Drive.urls')),
-    path('employer/',include('employer.urls'))
+    path('employer/',include('employer.urls')),
+    path('',RedirectView.as_view(url='/drive/'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
