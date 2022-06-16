d3914521c9b487eedde9892df76260d213b8df48
Mr-Tintin <esteban.descieux@free.fr>
2022-06-16 16:05:06 -0400

    Serv update

--- DriveProject/employer/models.py
+++ DriveProject/employer/models.py
@@ -12,7 +12,7 @@ class Categories(models.Model):
 class Produits(models.Model):
     nom = models.CharField(max_length=30,null=False)
     date_per = models.DateField(null=False)
-    photo = models.ImageField(blank=True)
+    photo = models.ImageField(blank=False)
     marque = models.CharField(max_length=30)
     categorie = models.ForeignKey(Categories,on_delete=models.SET_NULL,null=True)
     stock = models.PositiveIntegerField()
